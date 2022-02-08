e91f3fb18724c31d39e6de1a7164ecad5a7deef9
iyinraphael <iyinraphael@gmail.com>
2022-02-08 15:56:10 -0500

    model, service and repository set up

---
+++ Supermarket.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var catergories = await _categoryService.ListAsync();
+            return catergories;
+        }
+    }
+}
